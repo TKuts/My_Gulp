b988ebe830a540a3d8b0b5acaafa6eea07822aef
Anton Kuts <anton.a.kuts@gmail.com>
2023-08-08 22:54:59 +0300

    added adaptation and compression to images

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,42 @@ const uglify = require("gulp-uglify-es").default;
 const browserSync = require("browser-sync").create();
 const autoprefixer = require("gulp-autoprefixer");
 const clean = require("gulp-clean");
+const avif = require("gulp-avif");
+const webp = require("gulp-webp");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+const svgSprite = require("gulp-svg-sprite");
+
+function images() {
+  return src(["app/images/src/*.*", "!app/images/src/*.svg"])
+    .pipe(newer("app/images/dist"))
+    .pipe(avif({ quality: 50 }))
+
+    .pipe(src("app/images/src/*.*"))
+    .pipe(newer("app/images/dist"))
+    .pipe(webp())
+
+    .pipe(src("app/images/src/*.*"))
+    .pipe(newer("app/images/dist"))
+    .pipe(imagemin())
+
+    .pipe(dest("app/images/dist"));
+}
+
+function sprite() {
+  return src("app/images/dist/*.svg")
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: "../sprite.svg",
+            example: true,
+          },
+        },
+      })
+    )
+    .pipe(dest("app/images/dist"));
+}
 
 function scripts() {
   return src("app/js/main.js")
@@ -25,17 +61,15 @@ function styles() {
 }
 
 function watching() {
-  watch(["app/scss/style.scss"], styles);
-  watch(["app/js/main.js"], scripts);
-  watch(["app/*.html"]).on("change", browserSync.reload);
-}
-
-function browsersync() {
   browserSync.init({
     server: {
       baseDir: "app/",
     },
   });
+  watch(["app/scss/style.scss"], styles);
+  watch(["app/images/src"], images);
+  watch(["app/js/main.js"], scripts);
+  watch(["app/*.html"]).on("change", browserSync.reload);
 }
 
 function cleanDist() {
@@ -43,16 +77,24 @@ function cleanDist() {
 }
 
 function building() {
-  return src(["app/css/style.min.css", "app/js/main.min.js", "app/**/*.html"], {
-    base: "app",
-  }).pipe(dest("dist"));
+  return src(
+    [
+      "app/css/style.min.css",
+      "app/images/dist/*.*",
+      "app/js/main.min.js",
+      "app/**/*.html",
+    ],
+    {
+      base: "app",
+    }
+  ).pipe(dest("dist"));
 }
 
 exports.styles = styles;
+exports.images = images;
+exports.sprite = sprite;
 exports.scripts = scripts;
 exports.watching = watching;
-exports.browsersync = browsersync;
-
 
 exports.build = series(cleanDist, building);
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(styles, images, scripts, watching);
