4187589efaafb10a519f7d90c09e2a78fcb8b9b2
Anton Kuts <anton.a.kuts@gmail.com>
2023-08-09 22:55:09 +0300

    the final build of Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -11,25 +11,51 @@ const webp = require("gulp-webp");
 const imagemin = require("gulp-imagemin");
 const newer = require("gulp-newer");
 const svgSprite = require("gulp-svg-sprite");
+const fonter = require("gulp-fonter");
+const ttf2woff2 = require("gulp-ttf2woff2");
+const include = require("gulp-include");
+
+function pages() {
+  return src("app/pages/*.html")
+    .pipe(
+      include({
+        includePaths: "app/components",
+      })
+    )
+    .pipe(dest("app"))
+    .pipe(browserSync.stream());
+}
+
+function fonts() {
+  return src("app/fonts/src/*.*")
+    .pipe(
+      fonter({
+        formats: ["woff", "ttf"],
+      })
+    )
+    .pipe(src("app/fonts/*.ttf"))
+    .pipe(ttf2woff2())
+    .pipe(dest("app/fonts"));
+}
 
 function images() {
   return src(["app/images/src/*.*", "!app/images/src/*.svg"])
-    .pipe(newer("app/images/dist"))
+    .pipe(newer("app/images"))
     .pipe(avif({ quality: 50 }))
 
     .pipe(src("app/images/src/*.*"))
-    .pipe(newer("app/images/dist"))
+    .pipe(newer("app/images"))
     .pipe(webp())
 
     .pipe(src("app/images/src/*.*"))
-    .pipe(newer("app/images/dist"))
+    .pipe(newer("app/images"))
     .pipe(imagemin())
 
-    .pipe(dest("app/images/dist"));
+    .pipe(dest("app/images"));
 }
 
 function sprite() {
-  return src("app/images/dist/*.svg")
+  return src("app/images/*.svg")
     .pipe(
       svgSprite({
         mode: {
@@ -40,7 +66,7 @@ function sprite() {
         },
       })
     )
-    .pipe(dest("app/images/dist"));
+    .pipe(dest("app/images"));
 }
 
 function scripts() {
@@ -69,6 +95,7 @@ function watching() {
   watch(["app/scss/style.scss"], styles);
   watch(["app/images/src"], images);
   watch(["app/js/main.js"], scripts);
+  watch(["app/components/*", "app/pages/*"], pages);
   watch(["app/*.html"]).on("change", browserSync.reload);
 }
 
@@ -80,7 +107,11 @@ function building() {
   return src(
     [
       "app/css/style.min.css",
-      "app/images/dist/*.*",
+      "!app/images/**/*.html",
+      "app/images/*.*",
+      "!app/images/*.svg",
+      "app/images/sprite.svg",
+      "app/fonts/*.*",
       "app/js/main.min.js",
       "app/**/*.html",
     ],
@@ -92,9 +123,12 @@ function building() {
 
 exports.styles = styles;
 exports.images = images;
+exports.fonts = fonts;
+exports.pages = pages;
+exports.building = building;
 exports.sprite = sprite;
 exports.scripts = scripts;
 exports.watching = watching;
 
 exports.build = series(cleanDist, building);
-exports.default = parallel(styles, images, scripts, watching);
+exports.default = parallel(styles, images, scripts, pages, watching);
